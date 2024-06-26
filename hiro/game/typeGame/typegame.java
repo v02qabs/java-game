355b61405b79319ba8c575f1001420b0fbf8b959
v02qabs <takesue090@gmail.com>
2024-06-26 23:56:49 +0900

    add typeGame.class

---
+++ hiro/game/typeGame/typegame.java
@@ -0,0 +1,16 @@
+package hiro.game.typeGame;
+
+import javax.swing.*;
+
+
+public class typegame extends JFrame{
+	public static void main(String[] args){
+		System.out.println("Hello　typing game.");
+	}
+	public void typegame(){
+		System.out.println("select game: " + "typegame");
+	}
+
+
+}
+
